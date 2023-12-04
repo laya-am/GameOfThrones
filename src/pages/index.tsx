63b797cc86501b876b4f34d6ad2aaa4309228470
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-12-04 15:14:01 +0100

    style quote page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Navbar from '../../components/Navbar'
+import styles from "../styles/Home.module.css"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel="icon" href="" /> */}
       </Head>
-      {/* <main className={`${styles.main} ${inter.className}`}> */}
+      <h1 className={styles.title}>Game of Thrones</h1>
     </>
   )
 }
