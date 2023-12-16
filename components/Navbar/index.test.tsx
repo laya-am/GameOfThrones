77dbfdbf0f8fe0729ca1cb810ad1bbc806c461d7
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-12-16 16:13:33 +0100

    add tests to Navbar component

---
+++ components/Navbar/index.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Navbar from './index';
+import '@testing-library/jest-dom';
+
+// Mock Next.js Link
+jest.mock('next/link', () => {
+  return ({ children, href }) => {
+    return <a href={href}>{children}</a>;
+  };
+});
+
+describe('Navbar Component', () => {
+  test('renders Navbar with links to Houses, Persons, and Quotes', () => {
+    render(<Navbar />);
+
+    const housesLink = screen.getByRole('link', { name: /Houses/i });
+    expect(housesLink).toBeInTheDocument();
+    expect(housesLink).toHaveAttribute('href', '/houses');
+
+    const personsLink = screen.getByRole('link', { name: /Persons/i });
+    expect(personsLink).toBeInTheDocument();
+    expect(personsLink).toHaveAttribute('href', '/persons');
+
+    const quotesLink = screen.getByRole('link', { name: /Quotes/i });
+    expect(quotesLink).toBeInTheDocument();
+    expect(quotesLink).toHaveAttribute('href', '/quotes');
+  });
+});
